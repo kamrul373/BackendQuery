b53a674cf8bdc936a50ed8a32d873d1131c17ff1
unknown <textile.zero@gmail.com>
2023-01-08 23:27:35 +0600

    longest runTime movies api added

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require( "express" )
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require( "cors" )
+app.use( cors() )
+app.use( express.json() );
+
+// db connection
+const mysql = require( 'mysql' );
+
+var connection = mysql.createConnection( {
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'backend'
+} );
+
+connection.connect();
+
+async function run () {
+	app.get( "/api/v1/longest-duration-movies", ( req, res ) => {
+		const result = connection.query( 'SELECT * FROM movies ORDER BY runtimeMinutes DESC limit 10', function ( error, results, fields ) {
+			if ( error ) throw error;
+			results = JSON.parse( JSON.stringify( results ) )
+			res.send( results );
+		} );
+
+	} )
+
+
+}
+run().catch( error => console.log( error ) )
+
+
+app.get( "/", ( req, res ) => {
+	res.send( "Onito Technologies Backend server task is running !" )
+} )
+
+app.listen( port, () => {
+	console.log( "Onito Technologies Private Limited backend server task is running at ", port )
+} )
